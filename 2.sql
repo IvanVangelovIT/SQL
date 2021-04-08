5de1a3883ff18668ce4ed43e071be59389ef4aa6
IRV <ivanrvangelovit@gmail.com>
2021-04-08 20:37:01 +0300

    Added SQL

---
+++ 2.sql
@@ -0,0 +1,8 @@
+TRUNCATE TABLE PEOPLE
+
+INSERT INTO People (Name, Picture, Height, [Weight], Gender, Birthdate, Biography) VALUES
+('Pesho Marinov', NULL, 1.12, 12.32, 'M', Convert(DateTime,'19921702',112), 'UNENPLOYED'),
+('Ivan Dimov', NULL, 2.33, 42.12, 'M', Convert(DateTime,'19921703',112), 'UNENPLOYED2'),
+('Todorka Peneva', NULL, 2.33, 23.12, 'M', Convert(DateTime,'19921704',112), 'UNENPLOYED3'),
+('Dilyana Ivanova', NULL, 2.33, 33.12, 'F', Convert(DateTime,'19921705',112), 'UNENPLOYED4'),
+('Todor Stamatov', NULL, 2.33, 32.11, 'F', Convert(DateTime,'19921706',112), 'UNENPLOYED5')
